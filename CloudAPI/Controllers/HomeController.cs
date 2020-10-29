a4114b41683df9b5254ea955fe3fd25b2af8e26e
awgustavo <awgustavo@gmail.com>
2020-10-29 14:19:15 -0300

    LiveMap v1.27

---
+++ CloudAPI/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CloudAPI.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "CloudAPI Running.";            
+        }
+    }
+}
